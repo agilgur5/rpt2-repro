7919d25f097f4e19138821a51bdb17fc1acba305
agilgur5 <agilgur5@gmail.com>
2022-06-08 14:30:36 -0400

    env: add comment about using `tsc` for type-checking in `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // run `npm run build` in the terminal to build this file with rpt2
+// run `npm run tsc` in the terminal to type-check this file tsc
 
 export default function sum(a: number, b: number) {
   return a + b;
